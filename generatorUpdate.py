4f246b7b2a29bb88b02f00d63531087ea1a72d0d
notcrummynuggets <84356098+notcrummynuggets@users.noreply.github.com>
2021-05-17 17:15:12 -0700

    Download this version

---
+++ generatorUpdate.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+ideologies = ["Anarchist", "Communist", "Conservitive", "Corporatist", "Democratic", "Environmentalist", "Fascist", "Liberalist", "Libertarian", "Nationalist", "Populist", "Socialist", "Posadist", "Fuedalist"]
+adgectives = ["Neo", "Classical", "Anarcho", "Post"]
+
+def derfinings():
+    global ideology1
+    global ideology2
+    global adgective
+    ideology1 = random.choice(ideologies)
+    ideology2 = random.choice(ideologies)
+    adgective = random.choice(adgectives)
+    if ideology1 == ideology2:
+        vars()
+
+def printing():
+    print("Calculating...")
+    time.sleep(0.5)
+    print("Generating..")
+    time.sleep(1)
+    print("Your Ideology is... " + adgective + "-" + ideology1 + "-" + ideology2 + "!")
+
+derfinings()
+printing()
